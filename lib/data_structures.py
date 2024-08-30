3e0e1a41c31ae23e9dca2670517d08a7c6ee6333
+ <+>
2024-08-30 20:21:06 +0300

    test done

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,35 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [food['name'] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food['heat_level'] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for spicy in spicy_foods:
+        hot_emoji = '🌶' * spicy['heat_level']
+        print(f"{spicy['name']} ({spicy['cuisine']}) | Heat Level: {hot_emoji}")
+    return None
+    
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    return next((spicy for spicy in spicy_foods if spicy['cuisine'] == cuisine), None)
+    
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for spicy in spicy_foods:
+        if spicy['heat_level'] > 5 :
+            hot_emoji = '🌶' * spicy['heat_level']
+            print(f"{spicy['name']} ({spicy['cuisine']}) | Heat Level: {hot_emoji}")
+    
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat_total = [spicy['heat_level'] for spicy in spicy_foods]
+    return sum(heat_total) / len(heat_total)
+    
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+    
